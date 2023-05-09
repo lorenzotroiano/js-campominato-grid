0b3d1f65bbce781b924288c31841c1a552512abc
lorenzotroiano <lorenzo.troiano1999@gmail.com>
2023-05-09 17:46:29 +0200

    migliroato codice con commenti

--- js/campominato.js
+++ js/campominato.js
@@ -1,28 +1,12 @@
 
 
 
-
+// variabile per selezionare nel dom il div che ha l'id "container"
+const container = document.getElementById("container");
 
 
 
 // L’utente clicca su un bottone che genererà una griglia di gioco quadrata.
-
-
-
-
-// Ogni cella ha un numero progressivo, da 1 a 100.
-
-
-// Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-
-
-// Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
-
-
-
-
-// variabile per selezionare nel dom il div che ha l'id "container"
-const container = document.getElementById("container");
 
 // creare constante per selezionare l'id del button
 const buttonGenera = document.getElementById("genera");
@@ -34,24 +18,37 @@ const buttonGenera = document.getElementById("genera");
 buttonGenera.addEventListener("click",
 function() {
 
+     // Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+
     for (let i = 1; i <= 100; i++) {
+
+       
 
         // creata una constante con dentro la funzione e i valori che devo creare "div" e aggiungere come classe "square"
         const nuovoQuadrato = createContainerSquare("div", "square")
     
+
+
+  // Ogni cella ha un numero progressivo, da 1 a 100.
+
+        // creato nuovo elemento, span.
+        const createSpan = document.createElement('span');
+        // aggiunto al nuovo elemento creato la variabile i (che genererà i numero da 1 a 100 all'interno dello span)
+        createSpan.append(i);
+
+
+// Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 
         // nuovo evento al click, ma questa volta solo sul quadrato creato, in modo da aggiungergli la classe che farà cambiare colore al click
         nuovoQuadrato.addEventListener("click", 
         function() {
 
             nuovoQuadrato.classList.add('blue');
+            console.log("Il quadrato selezionato è il numero:" + i);
         })
        
 
-        // creato nuovo elemento, span.
-        const createSpan = document.createElement('span');
-        // aggiunto al nuovo elemento creato la variabile i (che genererà i numero da 1 a 100 all'interno dello span)
-        createSpan.append(i);
+       
         
        
            
@@ -77,7 +74,7 @@ function() {
 
 
 
-// FUNZIONI
+// funzione per creare elementi e associargli una classe
 function createContainerSquare(prendiElement, prendiClass){
 
     const newElement = document.createElement(prendiElement);
